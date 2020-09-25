0630d80d4b74a442925d018baac24791dd3632b5
romerodevelopment.github.io <64169519+romerodevelopment@users.noreply.github.com>
2020-09-25 11:27:00 -0400

    Transiciones

--- app/src/components/curso-vue.vue
+++ app/src/components/curso-vue.vue
@@ -1,60 +1,31 @@
 <template>
   <div>
-      <template v-if="error">
-        <p>Error Form</p>
-      </template>
-
-      <form @submit.prevent="sentForm">
-        <input v-model="$v.name.$model" type="text">
-        <button>Enviar</button>
-        <p v-if="!$v.name.minLength"> Nombre debe ser mayor a {{$v.name.$params.minLength.min}}</p>
-      </form>
+      <button @click="show = !show">Toggle</button>
+      <transition name="fade">
+          <p v-if="show">Hello</p>
+      </transition>
   </div>
 </template>
  
 <script>
-import { required, minLength} from 'vuelidate/lib/validators'
 
 export default {
+  name: 'CursoVue',
   data() {
     return {
-      name: "",
-      error: false
+      show: true
     }
-  },
-
-  validations: {
-    name: {
-      required,
-      minLength: minLength(5)
-    }
-  },
-
-  watch: {
-    name: function(old_value, new_value) {
-      console.log("old " + old_value)
-      console.log("new " + new_value)
-    }
-  },
-
-  methods: {
-    sentForm: function() {
-      this.$v.$touch();
-
-      if(this.$v.$invalid) {
-        this.error = true;
-      }
-      else {
-        alert("Formulario valido")
-      }
-    }
-  } 
-
+  }
 }
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 2s;
+  }
 </style>
