b44a01d799c378b3a06ec5b5adb7501e0debcc80
romerodevelopment.github.io <64169519+romerodevelopment@users.noreply.github.com>
2020-09-25 00:20:53 -0400

    Vue Validate

--- app/src/components/curso-vue.vue
+++ app/src/components/curso-vue.vue
@@ -1,28 +1,57 @@
 <template>
   <div>
-    <a href="#">
-      <slot name="header">
-        Header
-      </slot>
-      <br>
-      <slot name="body">
-        Body
-      </slot>
-      <br>
-      <slot name="footer">
-        Footer
-      </slot>
-    </a>
+      <template v-if="error">
+        <p>Error Form</p>
+      </template>
+
+      <form @submit.prevent="sentForm">
+        <input v-model="$v.name.$model" type="text">
+        <button>Enviar</button>
+        <p v-if="!$v.name.minLength"> Nombre debe ser mayor a {{$v.name.$params.minLength.min}}</p>
+      </form>
   </div>
 </template>
  
 <script>
+import { required, minLength} from 'vuelidate/lib/validators'
+
 export default {
-  name: 'CursoVue',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      name: "",
+      error: false
+    }
+  },
+
+  validations: {
+    name: {
+      required,
+      minLength: minLength(5)
+    }
+  },
+
+  watch: {
+    name: function(old_value, new_value) {
+      console.log("old " + old_value)
+      console.log("new " + new_value)
+    }
+  },
+
+  methods: {
+    sentForm: function() {
+      this.$v.$touch();
+
+      if(this.$v.$invalid) {
+        this.error = true;
+      }
+      else {
+        alert("Formulario valido")
+      }
+    }
+  } 
+
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
